bd4214afd1fb214d32adae791a68565ab26e7040
Rajendra <rajagokhale@hotmail.com>
2022-12-25 16:46:38 +0530

    second commit

--- src/main/java/com/banking/application/dev/Login.java
+++ src/main/java/com/banking/application/dev/Login.java
@@ -6,11 +6,14 @@ public class Login {
 		
 		String userName = "admin";
 		String password = "admin@123";
+		int failCnt = 0;
 		
 		if (userName.equals("admin") && password.equals("admin@123")) {
 			System.out.println("Valid Login credentials.");
+			System.out.println("Navigate user to Home page.");
 		} else {
 			System.out.println("Invalid Login credentials.");
+			failCnt += 1;
 		}
 
 	}
